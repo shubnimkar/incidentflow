3e70d2592eed366fd5771dfd05772eaf047b6adf
shubnimkar <nimkarshubham23@gmail.com>
2025-06-13 16:43:06 +0530

    Add admin panel with role-based access control and not authorized page

---
+++ frontend/client/src/utils/auth.js
@@ -0,0 +1,12 @@
+// src/utils/auth.js
+export const getCurrentUser = () => {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    return payload; // { id, email, role, iat, exp }
+  } catch (err) {
+    console.error("Invalid token");
+    return null;
+  }
+};
