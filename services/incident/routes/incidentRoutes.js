1d22f58284ae4abd8b76c0db2cb7847f14e7ea30
shubnimkar <nimkarshubham23@gmail.com>
2025-06-13 13:32:49 +0530

    Implement admin role verification and enhance user role management

--- services/incident/routes/incidentRoutes.js
+++ services/incident/routes/incidentRoutes.js
@@ -3,18 +3,19 @@ const router = express.Router();
 const { createIncident, getAllIncidents, updateIncidentStatus,assignIncident, updateIncident, addComment, getIncidentById} = require("../controllers/incidentController");
 //const verifyToken = require("../middleware/authMiddleware");
 const verifyToken = require("../middleware/auth");
+const requireAdmin = require("../middleware/admin");
 
+// Public routes (any logged-in user)
 
 router.post("/", verifyToken, createIncident);
 router.get("/", verifyToken, getAllIncidents);
-router.put("/:id", verifyToken, updateIncidentStatus);
-router.patch("/:id/assign", verifyToken, assignIncident);
-router.patch("/:id", verifyToken, updateIncident);
-router.post("/:id/comments", verifyToken, addComment);
 router.get("/:id", verifyToken, getIncidentById);
+router.post("/:id/comments", verifyToken, addComment);
 
-
-
+// Admin routes (admin only)
+router.put("/:id", verifyToken, requireAdmin, updateIncidentStatus);
+router.patch("/:id/assign", verifyToken,requireAdmin, assignIncident);
+router.patch("/:id", verifyToken,requireAdmin, updateIncident);
 
 
 module.exports = router;
--- services/user/models/User.js
+++ services/user/models/User.js
@@ -5,6 +5,11 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["responder", "admin"],
+    default: "responder", // 👈 default role on registration
+  },
 });
 
 userSchema.pre("save", async function (next) {
